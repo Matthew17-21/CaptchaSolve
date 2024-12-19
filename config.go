492691429fe9f3b9dc609cf7a2e04aaf9049e48b
Matthew Lugo <matthewlugo746@gmail.com>
2024-12-19 18:21:35 -0500

    Added default config

--- config.go
+++ config.go
@@ -2,7 +2,16 @@ package captchasolve
 
 import captchatools "github.com/Matthew17-21/Captcha-Tools/captchatools-go"
 
+const defaultMaxCapacity = 25
+
 type config struct {
 	maxCapacity int // The max amount of captcha tokens to be held
 	harvesters  []captchatools.Harvester
+}
+
+func defaultConfig() config {
+	return config{
+		maxCapacity: defaultMaxCapacity,
+		harvesters:  make([]captchatools.Harvester, 0),
+	}
 }
